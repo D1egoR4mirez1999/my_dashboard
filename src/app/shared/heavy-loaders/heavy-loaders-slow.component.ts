acc224ddcba306dd24ecfee67ac2a7d282aa735b
Diego Ramirez <dialrasa2@gmail.com>
2025-01-09 19:52:57 -0500

    refactor: main components for the app

---
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-slow',
+  imports: [],
+  template: `Hello world`,
+})
+export class HeavyLoadersSlowComponent {
+
+}
---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-loader',
+  imports: [],
+  template: `Hello world`,
+})
+export class UserLoaderComponent {
+
+}
