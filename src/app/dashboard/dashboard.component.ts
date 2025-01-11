16e686df3fe7fcf60afaa692b3fdf56faed2c7e3
Diego Ramirez <dialrasa2@gmail.com>
2025-01-11 16:15:24 -0500

    feat: add routes in side-menu.component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { SideMenuComponent } from '../shared/side-menu/side-menu.component';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [
+    RouterModule,
+    SideMenuComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
-
+  routes = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
 }
